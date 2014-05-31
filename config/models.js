e99d75a4664400059d0acb7eceb7c56d443f19b1
DSW41923 <dsw41923@gmail.com>
2014-05-31 13:17:16 +0000

    Last CLass

---
+++ config/models.js
@@ -0,0 +1,23 @@
+/**
+* Created with week5.
+* User: DSW41923
+* Date: 2014-05-24
+* Time: 10:52 AM
+* To change this template use Tools | Templates.
+*/
+/**
+ * Default model configuration
+ * (sails.config.models)
+ *
+ * Unless you override them, the following properties will be included
+ * in each of your models.
+ */
+
+module.exports.models = {
+
+  // Your app's default connection.
+  // i.e. the name of one of your app's connections (see `config/connections.js`)
+  //
+  // (defaults to localDiskDb)
+  connection: 'mysql'
+};
